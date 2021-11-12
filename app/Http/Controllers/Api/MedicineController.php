68b8d9e141961f51317f7555006be9816808beb9
Fernando Tzoc <fernandotzoc4@gmail.com>
2021-11-11 23:29:35 -0600

    Api filtrar por grupos

--- app/Http/Controllers/Api/MedicineController.php
+++ app/Http/Controllers/Api/MedicineController.php
@@ -13,7 +13,7 @@ class MedicineController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    /*     public function index()
     {
         
         $count = Medicine::count();
@@ -24,6 +24,44 @@ class MedicineController extends Controller
             'medicines' => $medicines,
         ]);
 
+    } */
+
+    public function index(Request $request)
+    {
+
+        $group = $request->get('group');
+
+        $query = Medicine::join('sub_classifications', 'sub_classifications.id', '=', 'medicines.sub_classification_id')
+            ->join('classifications', 'classifications.id', '=', 'sub_classifications.classification_id')
+            ->join('groups', 'groups.id', '=', 'classifications.group_id')
+            ->select(
+                'groups.letter as letterGroup',
+                'groups.name as nameGroup',
+                'groups.description as descriptionGroup',
+                'classifications.code as codeClassification',
+                'classifications.name as nameClassification',
+                'classifications.additional as additionalClassification',
+                'sub_classifications.code as codeSubClassification',
+                'sub_classifications.name as nameSubClassification',
+                'sub_classifications.additional as additionalSubClassification',
+                'medicines.active_principle as activePrincipleMed',
+                'medicines.pharmaceutical_form as pharmaceuticalFormMed',
+                'medicines.indications as indicationsMed',
+                'medicines.route_dosage as routeDosageMed',
+                'medicines.management_rules as managementRulesMed',
+                'medicines.observations as observationsMed',
+                'medicines.additional as additionalMed'
+            );
+
+        if ($request->has('group')) {
+            $query = $query->where('groups.letter', '=', $group);
+        }
+
+        $medicines = $query->get();
+
+        return response()->json([
+            'medicines' => $medicines,
+        ]);
     }
 
     /**
