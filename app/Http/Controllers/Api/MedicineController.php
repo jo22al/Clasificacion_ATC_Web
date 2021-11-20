9aa7808693f34a333123c0afdee13a3a34d8ef46
Fernando Tzoc <fernandotzoc4@gmail.com>
2021-11-20 00:52:22 -0600

    ApiMedicineController Optimizado

--- app/Http/Controllers/Api/MedicineController.php
+++ app/Http/Controllers/Api/MedicineController.php
@@ -8,51 +8,12 @@ use Illuminate\Http\Request;
 
 class MedicineController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    /*     public function index()
-    {
-        
-        $count = Medicine::count();
-        $medicines = Medicine::with('sub_classification')->get();
-    
-        return response()->json([
-            'total' => $count,
-            'medicines' => $medicines,
-        ]);
-
-    } */
 
     public function index(Request $request)
     {
 
         $group = $request->get('group');
-
-        $query = Medicine::join('sub_classifications', 'sub_classifications.id', '=', 'medicines.sub_classification_id')
-            ->join('classifications', 'classifications.id', '=', 'sub_classifications.classification_id')
-            ->join('groups', 'groups.id', '=', 'classifications.group_id')
-            ->select(
-                'groups.letter as letterGroup',
-                'groups.name as nameGroup',
-                'groups.description as descriptionGroup',
-                'classifications.code as codeClassification',
-                'classifications.name as nameClassification',
-                'classifications.additional as additionalClassification',
-                'sub_classifications.code as codeSubClassification',
-                'sub_classifications.name as nameSubClassification',
-                'sub_classifications.additional as additionalSubClassification',
-                'medicines.id as idMedicine',
-                'medicines.active_principle as activePrincipleMed',
-                'medicines.pharmaceutical_form as pharmaceuticalFormMed',
-                'medicines.indications as indicationsMed',
-                'medicines.route_dosage as routeDosageMed',
-                'medicines.management_rules as managementRulesMed',
-                'medicines.observations as observationsMed',
-                'medicines.additional as additionalMed'
-            );
+        $query = Medicine::queryApi();
 
         if ($request->has('group')) {
             $query = $query->where('groups.letter', '=', $group);
@@ -68,29 +29,8 @@ class MedicineController extends Controller
     public function search($query)
     {
 
-        $medicines = Medicine::join('sub_classifications', 'sub_classifications.id', '=', 'medicines.sub_classification_id')
-            ->join('classifications', 'classifications.id', '=', 'sub_classifications.classification_id')
-            ->join('groups', 'groups.id', '=', 'classifications.group_id')
-            ->select(
-                'groups.letter as letterGroup',
-                'groups.name as nameGroup',
-                'groups.description as descriptionGroup',
-                'classifications.code as codeClassification',
-                'classifications.name as nameClassification',
-                'classifications.additional as additionalClassification',
-                'sub_classifications.code as codeSubClassification',
-                'sub_classifications.name as nameSubClassification',
-                'sub_classifications.additional as additionalSubClassification',
-                'medicines.id as idMedicine',
-                'medicines.active_principle as activePrincipleMed',
-                'medicines.pharmaceutical_form as pharmaceuticalFormMed',
-                'medicines.indications as indicationsMed',
-                'medicines.route_dosage as routeDosageMed',
-                'medicines.management_rules as managementRulesMed',
-                'medicines.observations as observationsMed',
-                'medicines.additional as additionalMed'
-            )->where('medicines.active_principle', 'like', '%' . $query . '%')->get();
-
+        $medicines = Medicine::queryApi()->where('medicines.active_principle', 'like', '%' . $query . '%')
+                                         ->orwhere('medicines.indications', 'like', '%' . $query . '%')->get();;
 
         return response()->json([
             'medicines' => $medicines,
